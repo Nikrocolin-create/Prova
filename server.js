a82eaea83461719c3006c31a6cfd682a2740065c
Nikita <9nik812@gmail.com>
2023-10-02 09:04:24 +0200

    server data

--- server.js
+++ server.js
@@ -5,16 +5,41 @@ const app = express();
 app.use(cors());
 
 app.use('/login', (req, res) => {
-    console.log(req)
     res.send({
       token: 'test123',
     });
   });
 
 app.use('/rate/:postId', (req, res) => {
-    console.log(req)
     res.send({
       token: 'variabel',
     });
   });
+
+let MOCK_DATA = [
+  {clientId:"1",date:"", satisfaction:5, recommend:true,id:"1"},
+  {clientId:"1",date:"", satisfaction:5, recommend:true,id:"2"},
+  {clientId:"1",date:"", satisfaction:6, recommend:true,id:"3"},
+  {clientId:"2",date:"", satisfaction:1, recommend:false,id:"4"},
+  {clientId:"2",date:"", satisfaction:4, recommend:true,id:"5"},
+  {clientId:"2",date:"", satisfaction:1, recommend:true,id:"6"},
+  {clientId:"3",date:"", satisfaction:1, recommend:false,id:"7"},
+  {clientId:"3",date:"", satisfaction:5, recommend:true,id:"8"},
+  {clientId:"4",date:"", satisfaction:9, recommend:true,id:"9"},
+  {clientId:"5",date:"", satisfaction:1, recommend:true,id:"10"}
+]
+for (let i = 0; i < 10; i++) {
+  let date = new Date();
+  date.setDate(i)
+  
+  const options = {year: 'numeric', month: 'numeric', day: 'numeric' };
+  MOCK_DATA[i]["date"] = date.toLocaleDateString('en-us',options)
+}
+
+app.use('/data/get', (req, res) => {
+  res.send({
+    data: MOCK_DATA
+  });
+});
+
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,45 +5,68 @@ import Average from './Average/Average';
 import TrendChart from './TrendChart/TrendChart';
 import PieChartYear from './PieChartYear/PieChartYear';
 import Satisfied from './Satisfied/Satisfied';
+import { useState, useEffect} from 'react';
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 export default function Dashboard() {
-  let MOCK_DATA = [
-    {clientId:"1",date:"", satisfaction:5, recommend:true,id:"1"},
-    {clientId:"1",date:"", satisfaction:5, recommend:true,id:"2"},
-    {clientId:"1",date:"", satisfaction:6, recommend:true,id:"3"},
-    {clientId:"2",date:"", satisfaction:1, recommend:false,id:"4"},
-    {clientId:"2",date:"", satisfaction:4, recommend:true,id:"5"},
-    {clientId:"2",date:"", satisfaction:1, recommend:true,id:"6"},
-    {clientId:"3",date:"", satisfaction:1, recommend:false,id:"7"},
-    {clientId:"3",date:"", satisfaction:5, recommend:true,id:"8"},
-    {clientId:"4",date:"", satisfaction:9, recommend:true,id:"9"},
-    {clientId:"5",date:"", satisfaction:1, recommend:true,id:"10"}
-  ]
-  // for (let i = 0; i < 10; i++) {
-  //   MOCK_DATA[i]["clientId"] = getRandomInt(10)
-  // }
-  for (let i = 0; i < 10; i++) {
-    let date = new Date();
-    date.setDate(i)
-    
-    const options = {year: 'numeric', month: 'numeric', day: 'numeric' };
-    MOCK_DATA[i]["date"] = date.toLocaleDateString('en-us',options)
-  }
-  console.log(MOCK_DATA)
+  const [dashoboardData, setDashboardData] = useState([])
 
-  return(
-<div className={classes.gridContainer}>
-<div className={classes.chartContainer + " " + classes.chart1}><LikelyToRecommend data={MOCK_DATA}/> </div>
-<div className={classes.chartContainer + " " + classes.chart2}> <Satisfied data={MOCK_DATA}/> </div>
-<div className={classes.chartContainer + " " + classes.chart3}> <Average data={MOCK_DATA}/></div>
-<div className={classes.chartContainer + " " + classes.chart4}><PieChartYear data={MOCK_DATA}/></div>
-<div className={classes.chartContainer + " " + classes.chart5}><TrendChart data={MOCK_DATA}/></div>
-</div>
-  );
+
+  useEffect(() => {
+    let fetchUserData = async () => {
+      await fetch("http://localhost:8080/data/get")
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          setDashboardData(data["data"])
+        })
+    }
+  
+    fetchUserData()
+  }, [])
+  // let MOCK_DATA = [
+  //   {clientId:"1",date:"", satisfaction:5, recommend:true,id:"1"},
+  //   {clientId:"1",date:"", satisfaction:6, recommend:true,id:"2"},
+  //   {clientId:"1",date:"", satisfaction:6, recommend:true,id:"3"},
+  //   {clientId:"2",date:"", satisfaction:10, recommend:false,id:"4"},
+  //   {clientId:"2",date:"", satisfaction:5, recommend:true,id:"5"},
+  //   {clientId:"2",date:"", satisfaction:5, recommend:true,id:"6"},
+  //   {clientId:"3",date:"", satisfaction:10, recommend:false,id:"7"},
+  //   {clientId:"3",date:"", satisfaction:5, recommend:true,id:"8"},
+  //   {clientId:"4",date:"", satisfaction:9, recommend:true,id:"9"},
+  //   {clientId:"5",date:"", satisfaction:10, recommend:true,id:"10"}
+  // ]
+  // // for (let i = 0; i < 10; i++) {
+  // //   MOCK_DATA[i]["clientId"] = getRandomInt(10)
+  // // }
+  // for (let i = 0; i < 10; i++) {
+  //   let date = new Date();
+  //   date.setDate(i)
+    
+  //   const options = {year: 'numeric', month: 'numeric', day: 'numeric' };
+  //   MOCK_DATA[i]["date"] = date.toLocaleDateString('en-us',options)
+  // }
+  // console.log(MOCK_DATA)
+  if (dashoboardData.length > 0) {
+    return(
+      <div className={classes.gridContainer}>
+        <div className={classes.chartContainer + " " + classes.chart1}><LikelyToRecommend data={dashoboardData}/> </div>
+        <div className={classes.chartContainer + " " + classes.chart2}> <Satisfied data={dashoboardData}/> </div>
+        <div className={classes.chartContainer + " " + classes.chart3}> <Average data={dashoboardData}/></div>
+        <div className={classes.chartContainer + " " + classes.chart4}><PieChartYear data={dashoboardData}/></div>
+        <div className={classes.chartContainer + " " + classes.chart5}><TrendChart data={dashoboardData}/></div>
+      </div>
+    );
+  } else {
+    return (
+      <div>Waiting data...</div>
+    )
+  }
+
 }
 
 //import { PieChart } from '@mui/x-charts';
